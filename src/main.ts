a9cf6daf654bf7a91a5fe2a7106b7f043b290c35
jdeepee <joshuadparkin@gmail.com>
2021-10-29 16:02:13 +0100

    plot multiple DNA's at once and switch to typescript

--- src/main.ts
+++ src/main.ts
@@ -2,28 +2,50 @@ const conductor = require("@holochain/conductor-api");
 const fs = require("fs");
 
 const targetDNA = "842d242e512f664b8dc4c939f965ba7d91c2b209699496e7c6cdaabda188f70055d4e018027086";
+const agentDNA = "842d24fbd991bc77a99d770cde64b673d343a8aef79468682bd3c7cb599913ca9ddd499a4eb4d0";
+const shortFormDNA = "842d24544f2cd5c8ef123a7348e44f243bdcc0a5729a2a61f9c3f6a467d03cf5cc714e6164fdd0";
+const whitelistedDNAS = [targetDNA, agentDNA, shortFormDNA];
 const waitTime = 10000;
 
-const iterations = 180;
+const iterations = 250;
 let counter = 0;
 
-const validation_limbo_series = [];
-const integration_limbo_series = [];
-const integrated_limbo_series = [];
-const timestamp_series = [];
+interface Result {
+    validation_limbo_series: number[]
+    integration_limbo_series: number[]
+    integrated_limbo_series: number[]
+    timestamp_series: Date[]
+    dna: string
+}
+
+const results : { [key:string] : Result; } = {};
 
 async function main() {
     try {
         const admin = await conductor.AdminWebsocket.connect(`http://localhost:2000`, 60000)
+        for (const dna of whitelistedDNAS) {
+            results[dna] = {
+                validation_limbo_series: [],
+                integration_limbo_series: [],
+                integrated_limbo_series: [],
+                timestamp_series: [],
+                dna: dna,
+            } as Result
+        }
         while (counter < iterations) {
             console.log("Running iteration", counter,"/",iterations);
-            const status = await getValidationStatus(admin, targetDNA)
-            console.log("Current status", status, "\n");
-            validation_limbo_series.push(status.validation_limbo);
-            integration_limbo_series.push(status.integration_limbo);
-            integrated_limbo_series.push(status.integrated);
-            timestamp_series.push(status.now)
-            const waitMs = waitTime - status.elapsed;
+            const gossip_status = await getValidationStatus(admin, whitelistedDNAS)
+            console.log("Current gossip status: \n", gossip_status, "\n");
+            for (const result of gossip_status) {
+                results[result.dna] = {
+                    validation_limbo_series: results[result.dna].validation_limbo_series.concat([result.validation_limbo]),
+                    integration_limbo_series: results[result.dna].integration_limbo_series.concat([result.integration_limbo]),
+                    integrated_limbo_series: results[result.dna].integrated_limbo_series.concat([result.integrated]),
+                    timestamp_series: results[result.dna].timestamp_series.concat([result.now]),
+                    dna: result.dna
+                } as Result
+            }
+            const waitMs = waitTime - gossip_status[0].elapsed;
             if (waitMs > 0) {
                 console.log("Sleeping", waitMs);
                 await sleep(waitMs)
@@ -31,19 +53,19 @@ async function main() {
             counter += 1;
         }
 
-        writeData(JSON.stringify(validation_limbo_series), "validation_limbo_series.json");
-        writeData(JSON.stringify(integration_limbo_series), "integration_limbo_series.json");
-        writeData(JSON.stringify(integrated_limbo_series), "integrated_limbo_series.json");
-        writeData(JSON.stringify(timestamp_series), "timestamp_series.json");
+        writeData(JSON.stringify(results), "results.json");
         sleep(5000);
         process.exit(1);
     } catch (e) {
         console.error(e);
+        //@ts-ignore
         throw new Error(e)
     }
 }
 
+//@ts-ignore
 function writeData(data, fileName) {
+    //@ts-ignore
     fs.writeFileSync(fileName, data, function(err) {
         if(err) {
             return console.log(err);
@@ -52,28 +74,35 @@ function writeData(data, fileName) {
     });    
 }
 
-async function getValidationStatus(adminConductor, dna) {
+//@ts-ignore
+async function getValidationStatus(adminConductor, dnas) {
     const start = new Date();
     const cells = await adminConductor.listCellIds();
+    const results = [];
     for (const cell of cells) {
-        if (dna != undefined && cell[0].toString("hex") == dna) {
+        if (dnas != undefined && dnas.includes(cell[0].toString("hex"))) {
+            const dna = dnas.find((dna: string) => dna == cell[0].toString("hex"))
             const now = new Date();
             const state = await adminConductor.dumpState({cell_id: cell});
             //delete state[0]["peer_dump"]["this_dna"]
             const integration_dump = state[0]["integration_dump"];
             const done = new Date();
+            //@ts-ignore
             const elapsed = done - start;
-            return {
+            results.push({
                 validation_limbo: integration_dump["validation_limbo"],
                 integration_limbo: integration_dump["integration_limbo"],
                 integrated: integration_dump["integrated"],
                 elapsed: elapsed,
-                now: now
-            }
+                now: now,
+                dna: dna
+            })
         }
     }
+    return results
 }
 
+//@ts-ignore
 async function printPeers(adminConductor, dna) {
     const cells = await adminConductor.listCellIds();
     for (const cell of cells) {
@@ -86,6 +115,7 @@ async function printPeers(adminConductor, dna) {
     }
 }
 
+//@ts-ignore
 async function printPeersFull(adminConductor, dna) {
     const cells = await adminConductor.listCellIds();
     for (const cell of cells) {
@@ -102,6 +132,7 @@ async function printPeersFull(adminConductor, dna) {
     }
 }
 
+//@ts-ignore
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
---
+++ src/plot_multiple.py
@@ -0,0 +1,81 @@
+from datetime import datetime
+import dateutil.parser
+import matplotlib.pyplot as plt
+import json
+
+results = json.load(open('../results.json'))
+names = {"842d242e512f664b8dc4c939f965ba7d91c2b209699496e7c6cdaabda188f70055d4e018027086": "Social_Context_DNA", 
+    "842d24fbd991bc77a99d770cde64b673d343a8aef79468682bd3c7cb599913ca9ddd499a4eb4d0": "Agent_DNA", 
+    "842d24544f2cd5c8ef123a7348e44f243bdcc0a5729a2a61f9c3f6a467d03cf5cc714e6164fdd0": "ShortForm_DNA"}
+
+for key, value in results.items():
+    name = names[key]
+    value["timestamp_series"] = [dateutil.parser.isoparse(timestamp) for timestamp in value["timestamp_series"]]
+
+    # plotting the points
+    plt.plot(value["timestamp_series"], value["validation_limbo_series"], label="validation_limbo_series")
+    plt.plot(value["timestamp_series"], value["integration_limbo_series"], label="integration_limbo_series")
+    plt.plot(value["timestamp_series"], value["integrated_limbo_series"], label="integrated_limbo_series")
+
+    # naming the x axis
+    plt.xlabel('x - axis')
+    # naming the y axis
+    plt.ylabel('y - axis')
+    
+    # giving a title to my graph
+    plt.title('Full limbo state {}'.format(name))
+    
+    # function to show the plot
+    plt.legend(loc='best')
+    plt.xticks(rotation=45)
+    plt.savefig("../graphs/full_limbo_state_{}.jpg".format(name))
+    plt.clf()
+
+    # plotting the points
+    plt.plot(value["timestamp_series"], value["validation_limbo_series"], label="validation_limbo_series")
+
+    # naming the x axis
+    plt.xlabel('x - axis')
+    # naming the y axis
+    plt.ylabel('y - axis')
+    
+    # giving a title to my graph
+    plt.title('Validation limbo {}'.format(name))
+    
+    # function to show the plot
+    plt.legend(loc='best')
+    plt.xticks(rotation=45)
+    plt.savefig("../graphs/validation_limbo_series_{}.jpg".format(name))
+    plt.clf()
+
+    plt.plot(value["timestamp_series"], value["integration_limbo_series"], label="integration_limbo_series")
+
+    # naming the x axis
+    plt.xlabel('x - axis')
+    # naming the y axis
+    plt.ylabel('y - axis')
+    
+    # giving a title to my graph
+    plt.title('Integration limbo {}'.format(name))
+    
+    # function to show the plot
+    plt.legend(loc='best')
+    plt.xticks(rotation=45)
+    plt.savefig("../graphs/integration_limbo_series_{}.jpg".format(name))
+    plt.clf()
+
+    plt.plot(value["timestamp_series"], value["integrated_limbo_series"], label="integrated_limbo_series")
+
+    # naming the x axis
+    plt.xlabel('x - axis')
+    # naming the y axis
+    plt.ylabel('y - axis')
+    
+    # giving a title to my graph
+    plt.title('Integrated limbo {}'.format(name))
+    
+    # function to show the plot
+    plt.legend(loc='best')
+    plt.xticks(rotation=45)
+    plt.savefig("../graphs/integrated_limbo_series_{}.jpg".format(name))
+    plt.clf()
